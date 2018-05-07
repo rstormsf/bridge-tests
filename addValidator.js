2413e3b1f6a1c281afdbfcfaddf1606c4f8a5c6c
Roman Storm <rstormsf@gmail.com>
2018-05-06 22:54:25 -0700

    update addvalidator

--- addValidator.js
+++ addValidator.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const fs = require('fs')
 const assert = require('assert');
 
-const {web3Foreign, Web3Utils} = require('./web3');
+const {web3Foreign, web3Home, Web3Utils} = require('./web3');
 const {getNonce, sendTx} = require('./helpers');
 const FOREIGN_ABI = require('./abis/foreign.abi');
 const VALIDATOR_ABI = require('./abis/validators.abi');
@@ -14,17 +14,34 @@ const {
   FOREIGN_BRIDGE_ADDRESS,
   FOREIGN_RPC_URL,
   TIMES,
-  NEW_VALIDATOR
+  NEW_VALIDATOR,
+  NETWORK,
+  HOME_RPC_URL,
+  HOME_BRIDGE_ADDRESS
 } = process.env;
-let foreign = new web3Foreign.eth.Contract(FOREIGN_ABI, FOREIGN_BRIDGE_ADDRESS);
-
 
 async function main() {
-  const validatorsAddress = await foreign.methods.validatorContract().call();
-  let validatorContract = new web3Foreign.eth.Contract(VALIDATOR_ABI, validatorsAddress);
+  let RPC_URL, BRIDGE_ADDRESS, web3
+  switch(NETWORK) {
+    case 'home':
+      RPC_URL = HOME_RPC_URL;
+      BRIDGE_ADDRESS = HOME_BRIDGE_ADDRESS;
+      web3 = web3Home
+      break;
+    case 'foreign':
+      RPC_URL = FOREIGN_RPC_URL;
+      BRIDGE_ADDRESS = FOREIGN_BRIDGE_ADDRESS;
+      web3 = web3Foreign
+      break;
+    default:
+      throw new Error('specify NETWORK!');
+  }
+  let bridge = new web3.eth.Contract(FOREIGN_ABI, BRIDGE_ADDRESS);
+  const validatorsAddress = await bridge.methods.validatorContract().call();
+  let validatorContract = new web3.eth.Contract(VALIDATOR_ABI, validatorsAddress);
   const owner = await validatorContract.methods.owner().call()
   assert.equal(owner.toLowerCase(), SENDER_ADDRESS.toLowerCase(), 'Sender address is not the owner');
-  let nonce = await getNonce({web3: web3Foreign, address: SENDER_ADDRESS });
+  let nonce = await getNonce({web3, address: SENDER_ADDRESS });
   const data = validatorContract.methods.addValidator(NEW_VALIDATOR).encodeABI();
   
     sendTx({
@@ -34,7 +51,7 @@ async function main() {
       value: '0',
       gasLimit: '251000',
       to: validatorsAddress,
-      rpcUrl: FOREIGN_RPC_URL,
+      rpcUrl: RPC_URL,
       gasPrice: '21'
 
     })
